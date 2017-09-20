853112f0cea772dd1781ec2b87c5e4d1a727acf3
beauregardhenry <beauregard.henry@gmail.com>
2017-09-19 21:28:01 -0600

    Working on Customer Model

--- src/app/models/customer.rb
+++ src/app/models/customer.rb
@@ -1,2 +1,36 @@
 class Customer < ApplicationRecord
+
+require "stripe"
+Stripe.api_key = # get value from Stripe, do not push your key
+
+  def create_customer
+    Stripe::Customer.create(
+      :id =>
+      :default_source => # from Stripe.js
+      :description =>
+      :email =>
+      :source => # token from Stripe.js
+    )
+  end
+
+  def retrieve_customer
+    Stripe::Customer.retrieve( {CUSTOMER_ID})
+  end
+
+  def update_customer
+    cu = Stripe::Customer.retrieve( {CUSTOMER_ID} )
+    cu.description = :description
+    cu.source = "token" # obtained with Stripe.js
+    cu.save
+  end
+
+  def delete_customer
+    cu = Stripe::Customer.retrieve( {CUSTOMER_ID})
+    cu.delete
+  end
+
+  def list_all_customers
+    Stripe::Customer.list
+  end
+
 end
